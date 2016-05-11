efd17a9beed269793c36d8ccc0ea96cbb7ab70e1
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2016-05-12 00:51:43 +0700

    add index page, and many assets

--- app/Http/Controllers/HomeController.js
+++ app/Http/Controllers/HomeController.js
@@ -1,10 +1,23 @@
 'use strict'
-
+var bcrypt = require('bcryptjs');
 class HomeController {
-
   * index (request, response) {
-    const view = yield response.view('index')
+	var salt = bcrypt.genSaltSync(10);
+	var hash = bcrypt.hashSync("B4c0/\/", salt);
+    const view = yield response.view('layout/home.html', {title: hash})
     response.send(view)
+  }
+
+  * home (request, response) {
+	var salt = bcrypt.genSaltSync(10);
+	var hash = bcrypt.hashSync("B4c0/\/", salt);
+    const view = yield response.view('layout/home.html', {title: hash})
+    response.send(view)
+  }
+
+  * tester (request, response){
+  	yield request.session.put('username', 'nipeharefa');
+  	response.json(yield request.session.all());
   }
 
 }
