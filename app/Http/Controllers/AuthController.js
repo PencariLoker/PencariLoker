fd70003192d976b275dab5da1bdfb52d96da959d
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2016-05-14 09:25:24 +0700

    auth linked

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -1,44 +1,23 @@
 'use strict'
-var nodeLinkedIn = require('node-linkedin')('75nb5nsun5gsp3', 'OKFiE0UtZNFHfk2p','http://localhost:3000/authentication');
-var linkedin;
-var logged_in = false;
-
+var Linkedin = require('node-linkedin')('75nb5nsun5gsp3', 'OKFiE0UtZNFHfk2p','http://localhost:3000/authentication');
 class AuthController {
 	* authentication(request, response) {
-		nodeLinkedIn.auth.authorize(response, ['r_basicprofile','r_emailaddress','w_share']);
+		Linkedin.auth.authorize(response, ['r_basicprofile','r_emailaddress','w_share']);
 	}
 	* accessToken(request, response) {
 		var tmp = request.get();
-		nodeLinkedIn.auth.getAccessToken(response, tmp.code, tmp.state, function(err, results) {
+		var cs = Linkedin.auth.getAccessToken(response, tmp.code, tmp.state, function(err, results) {
 	        if ( err )
 	            response.send(err,500);
-	        
-	        
-	        response.json(results);
-	    });
-		//response.json(tmp.code);
-		//response.json(req.all());
-		/*let getUser = this.getUserData(req, res);
-		nodeLinkedIn.auth.getAccessToken(res, req.get().code, req.get().state, function(err, results) {
-	        if ( err )
-	            return console.error(err);
-	        console.log(results);
-	        linkedin = nodeLinkedIn.init(results.access_token,{
-				 timeout: 60000
+
+	        var linkedin = Linkedin.init(results.access_token, {
+			    timeout: 10000
 			});
-	        return getUser.next();
-	    });*/
+			linkedin.people.me(function(err, $in) {
+	        	response.json($in);
+			});
+	    });
 	}
-	/** getUserData(request, response) {
-		linkedin.people.me(['id', 'first-name', 'last-name','headline','location:(name)','industry','num-connections'
-			,'summary','specialties','positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,industry))'
-			,'picture-url','picture-urls::(original)','site-standard-profile-request','api-standard-profile-request','public-profile-url'
-			], function(err, $in) {
-		    console.log($in);
-		    logged_in = true;
-		    return response.redirect('/');
-		});
-	}*/
 }
 
 module.exports = AuthController
