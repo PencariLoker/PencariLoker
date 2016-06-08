97b928533ff3f19e102675f7d32ec63f2f44c35f
Nipe Setiawan <nipeharefa@gmail.com>
2016-06-08 07:12:42 +0700

    Fix .gitignore

--- resources/assets/js/Admin/Users.js
+++ resources/assets/js/Admin/Users.js
@@ -8,23 +8,8 @@ var Users = require('./components/Users.vue');
 var Users_Add = require('./components/Users_add.vue');
 var Company_List = require('./components/Company_list.vue');
 var Company_Add = require('./components/Company_add.vue');
-
-
-// The router needs a root component to render.
-// For demo purposes, we will just use an empty one
-// because we are using the HTML as the app template.
 var App = Vue.extend({})
-
-// Create a router instance.
-// You can pass in additional options here, but let's
-// keep it simple for now.
 var router = new VueRouter()
-
-// Define some routes.
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
 router.map({
     '/' : {
         component : Home,
@@ -39,8 +24,4 @@ router.map({
         component: Company_Add, 
     }
 })
-
-// Now we can start the app!
-// The router will create an instance of App and mount to
-// the element matching the selector #app.
 router.start(App, '#app')
---
+++ resources/assets/js/Admin/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+	<navbar></navbar>	
+	<div id="page-wrapper">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard <small>Statistic Overview</small></h1>
+			</div>
+		</div>
+		<div class="row">
+			
+		</div>
+	</div>
+</template>
+
+
+<script type="text/javascript">
+	export default {
+		ready: function(){
+			$('title').text('Dashboard')
+			console.log("Home REady");
+		},
+		components: {
+			navbar: require('./_navbar.vue')
+		}
+	}
+</script>
