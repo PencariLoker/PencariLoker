19a1b4bc3f8aef81b9abffe7c006ca04d2a5ddec
root <ventzlienata@gmail.com>
2016-07-04 09:28:02 -0400

    Fix Login ADmin

--- app/Http/Controllers/AdminLoginController.js
+++ app/Http/Controllers/AdminLoginController.js
@@ -9,15 +9,21 @@ class AdminLoginController {
   * login (request, response){
     const email = request.input('email')
     const password = request.input('password')
-    if (email == 'admin@example.com' && password == 'admin') {
-      return response.json({status: 'ok'});
-    }
-    try {
-      const login = yield request.auth.attempt(email, password)
-    }
-    catch(UserNotFoundException) {
-      return response.json({status: 'INVALID_LOGIN'});
-    }
+    //if (email == 'admin@example.com' && password == 'admin') {
+     // return response.json({status: 'ok'});
+    //}
+    //try {
+      //const login = yield request.auth.attempt(email, password)
+    //}
+    //catch(UserNotFoundException) {
+      //return response.json({status: 'INVALID_LOGIN'});
+    //}
+    const login = yield request.auth.attempt(email, password)
+    
+    if (login)
+	return response.json({status: 'ok'});
+
+    return response.json({status: 'INVALID_LOGIN'});
   }
 }
 
